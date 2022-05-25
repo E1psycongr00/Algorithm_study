35ee575ddca47962373b25c4dd01a61ca0e0774a
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-25 11:16:22 +0900

    Error: 네트워크 구하기
    https://programmers.co.kr/learn/courses/30/lessons/43162
    
    전형적인 컴포넌트 구하기 문제이다. 그러나 에러가 뜬다....

---
+++ 임현규/programmers/Lv3/lv3_네트워크_중.py
@@ -0,0 +1,39 @@
+
+class UnionFind:
+    def __init__(self, size):
+        self.parent=  [i for i in range(size)]
+        self.rank = [1 for _ in range(size)]
+        
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+        if x != y:
+            if self.rank[x] > self.rank[y]:
+                self.parent[y] = x
+            elif self.rank[x] < self.rank[y]:
+                self.parent[x] = y
+            else:
+                self.rank[x] += 1
+                self.parent[y] = x
+                
+    def isConnected(self, x, y):
+        return self.find(x) == self.find(y)
+
+def solution(n, computers):
+    edges = []
+    for i in range(n):
+        for j in range(i+1, n):
+            if computers[i][j] == 1:
+                edges.append((i, j))
+            
+    count = n
+    uf = UnionFind(n)
+    for edge in edges:
+        uf.union(edge[0], edge[1])
+        count -= 1
+    return count
