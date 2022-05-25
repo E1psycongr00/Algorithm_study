98bc14963215311f9615966c35efeb1bcadb0cd2
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-25 11:21:40 +0900

    Solved(라인스위핑): 직각다각형
    https://www.acmicpc.net/problem/17611
    
    배열처리가 귀찮아서 딕셔너리로 바꿧다.
    
    나의 문제를 알아냈다. 바로 key를 정렬하지 않은 것이 문제다.
    
    누적합을 할 때 +1 -1 이렇게 하는 이유는 수직 수평선을 아래서 위로 왼쪽에서 오른쪽으로 밀면서 누적합을 구하고 특정 구간을 지나 버리게 되면 경계 끝 값을 빼고 설계하게 되어있었다.
    
    당연히 sort를 안하면 누적합 과정에서 문제가 생기니깐 틀릴 수 밖에 없는 것이다. sort를 고려해야 되면 key값도 뭔가 저장해서 푸는 습관을 길러야 할 것 같다.

--- 임현규/backjoon/라인스위핑/17611_직각다각형_상.py
+++ 임현규/backjoon/라인스위핑/17611_직각다각형_상.py
@@ -1,5 +1,5 @@
 import sys
-
+from collections import defaultdict
 input = sys.stdin.readline
 
 MAX_SIZE = 10 ** 6 + 10
@@ -8,16 +8,14 @@ n = int(input())
 x = [0] * (n)
 y = [0] * (n)
 
-xCnt = [0] * MAX_SIZE
-yCnt = [0] * MAX_SIZE
+xCnt = defaultdict(int)
+yCnt = defaultdict(int)
 
 for i in range(n):
     x[i], y[i] = map(int, input().split())
-    x[i] += 500001
-    y[i] += 500001
 
-x.append(x[0] + 500001)
-y.append(y[0] + 500001)
+x.append(x[0])
+y.append(y[0])
 
 for i in range(n):
     if y[i] == y[i + 1]:
@@ -25,14 +23,25 @@ for i in range(n):
         xmax = max(x[i], x[i + 1])
         xCnt[xmin] += 1
         xCnt[xmax] -= 1
-    elif x[i] == x[i+1]:
+    if x[i] == x[i+1]:
         ymin = min(y[i], y[i + 1])
         ymax = max(y[i], y[i + 1])
         yCnt[ymin] += 1
         yCnt[ymax] -= 1
 
-for i in range(1, MAX_SIZE):
-    xCnt[i] += xCnt[i - 1]
-    yCnt[i] += yCnt[i - 1]
+num_dict_x = sorted(xCnt.items(), key=(lambda x:x[0]))
+num_dict_y = sorted(yCnt.items(), key=(lambda x:x[0]))
 
-print(max(max(xCnt), max(yCnt)))
+xMax = 0
+xTmp = 0
+for k, v in num_dict_x:
+    xTmp += v
+    xMax = max(xMax, xTmp)
+
+yMax = 0
+yTmp = 0
+for k, v in num_dict_y:
+    yTmp += v
+    yMax = max(yMax, yTmp)
+
+print(max(xMax, yMax))
