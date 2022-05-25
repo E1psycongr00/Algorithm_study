177ddf5cd790ccf53a40bcab0dadefa002b23729
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-25 11:02:53 +0900

    Solved(구현): 소용돌이 예쁘게 출력하기
    https://www.acmicpc.net/problem/1022
    
    수학을 이용해 규칙을 찾아서 풀었다. 솔직히 기존 방법이 안된다는 게 찜찜하긴 하다. python이여서 느려서 그런건지 몰라도 시간 복잡도만 따졌을 때는 통과 할 수 있다고 생각했기 때문이다.
    
    특이사항은 문제에서 규칙을 찾아서 단순 시뮬레이션 구현이 아닌 값을 대입하는 형식으로 빠르다. 그리고 python 문법중에
    "a: >{size}" 문법이 있는데 이 뜻은 size 배열 크기에서 오른쪽 정렬을 하라는 것이다. 이 방법을 사용하면 예쁘게 출력 할 수 있다.

--- 임현규/backjoon/구현/1022_소용돌이예쁘게출력하기_상.py
+++ 임현규/backjoon/구현/1022_소용돌이예쁘게출력하기_상.py
@@ -1,41 +1,32 @@
 import sys
-
-
 input = sys.stdin.readline
 
-r1, c1, r2, c2 = map(int, input().split())
-board = [[0] * (c2 - c1 + 1) for _ in range(r2 - r1 + 1)]
-
-direction = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-
-move = 1
-d = 0
-
-y = 0
-x = 0
-cnt = 1
-r = r2 - r1
-c = c2 - c1
-maxLen = 0
-
-
-while True:
-    # 2 번의 방향 전환
-    for _ in range(2):
-        # move 번 움직임
-        for _ in range(move):
-            if r1 <= y <= r2 and c1 <= x <= c2:
-                board[y-r1][x-c1] = cnt
-            cnt += 1
-            y += direction[d][0]
-            x += direction[d][1]
-        d = (d + 1) % 4
-    move += 1
-    maxLen = len(str(cnt))
-    if (board[0][0] and board[r][0]) and board[r][c] and board[0][c]: break
-
-for i in range(r+1):
-    for j in range(c+1):
-        print(str(board[i][j]).rjust(maxLen, " "), end= " ")
-    print()
+def getValue(r, c):
+    n = max(abs(r), abs(c))
+    last = (2*n+1) ** 2
     
+    if r == n:
+        return last - (n - c)
+    if c == -n:
+        return last - 2*n - (n - r)
+    if r == -n:
+        return last - 4*n - (n + c)
+    if c == n:
+        return last - 6*n - (n + r)
+    
+r1, c1, r2, c2 = map(int, input().split())
+
+maxLen = 0
+for y in range(r1, r2+1):
+    for x in range(c1, c2+1):
+        maxLen = max(maxLen, len(str(getValue(y, x))))
+
+output = []
+
+for y in range(r1, r2 + 1):
+    for x in range(c1, c2 + 1):
+        output.append(f'{getValue(y, x): >{maxLen}} ')
+    output.append('\n')
+
+# 소용돌이 출력
+sys.stdout.write(''.join(output))
