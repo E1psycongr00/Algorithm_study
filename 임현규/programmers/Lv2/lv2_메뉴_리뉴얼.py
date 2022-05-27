482c9e856b335ee68dd8cf333747d7d178738847
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-05-27 15:28:22 +0900

    Solved(구현): 메뉴 리뉴얼
    https://programmers.co.kr/learn/courses/30/lessons/72411
    
    이 문제는 알파벳을 모두 등록하고 거기서 뽑아서 다시 비교 할 필요가 없다....
    
    정말 바보 같이 풀었는데 사실 그냥 순회 하면서 주어진 갯수 만큼 뽑아서 담아 둔 다음 갯수를 세서 많거나 중복되면 등록 해주면 된다.
    
    요즘 파이썬을 안써서 리스트 컴프리핸션이 딱딱 안떠오르는데 연습 좀 해둬야겠다.
    아까 for문 여러개 쓰니 자꾸 헷갈리고 실수하는데 리스트 컴프리핸션쓰면 그런 문제를 조금이나마 방지 할 수 있다.

--- 임현규/programmers/Lv2/lv2_메뉴_리뉴얼.py
+++ 임현규/programmers/Lv2/lv2_메뉴_리뉴얼.py
@@ -2,41 +2,14 @@ from itertools import combinations
 from collections import Counter, defaultdict
 
 
-def combInOrder(comb, order):
-    for c in comb:
-        if c not in order:
-            return False
-
-    return True
-
-
 def solution(orders, course):
-    res = []
-    alphabets = set()
-    for order in orders:
-        alphabets.update(order)
-
-    
-    for num in course:
-        counter = defaultdict(int)
-        for comb in set(combinations(alphabets, num)):
-            for order in orders:
-                if combInOrder(comb, order):
-                    counter[''.join(sorted(comb))] += 1
-        maxVal = 0
-        for key, v in sorted(counter.items(), key=lambda x: -x[1]):
-            print(key, v)
-            if v >= maxVal and v > 1:
-                maxVal = v
-                res.append(key)
-            else:
-                break
-
-    return sorted(res)
-
-print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
-
-
-# a = {"a":2, "b": 3, "ad": 5}
-#
-# print(sorted(a.items(), key=lambda x: x[1]))
+    answer = []
+    for c in course:
+        temp = []
+        for order in orders:
+            combi = combinations(sorted(order), c)
+            temp += combi
+        counter = Counter(temp)
+        if counter and max(counter.values()) > 1:
+            answer += [''.join(f) for f in counter if counter[f] == max(counter.values())]
+    return sorted(answer)
