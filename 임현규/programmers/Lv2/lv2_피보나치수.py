468ba0362e33f840c6421042f4ed4424e70a9284
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-07-25 23:25:36 +0900

    Solved(DP): 피보나치수
    https://school.programmers.co.kr/learn/courses/30/lessons/12945

---
+++ 임현규/programmers/Lv2/lv2_피보나치수.py
@@ -0,0 +1,10 @@
+MOD = 1234567
+
+def solution(n):
+    dp = [0] * 100001
+    dp[1] = 1
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        dp[i] %= MOD
+    
+    return dp[n]
