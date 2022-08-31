71c329a0ade1ddd41ee825ad60bb1c48a7ae4a79
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-08-31 12:36:00 +0900

    Solved(구현): 카펫
    https://school.programmers.co.kr/learn/courses/30/lessons/42842

--- 임현규/programmers/Lv2/lv2_카펫_하.py
+++ 임현규/programmers/Lv2/lv2_카펫_하.py
@@ -1,7 +1,22 @@
-def solution(brown, yellow):
-    for i in range(1, yellow+1):
-        if yellow % i == 0:
-            m = yellow // i
-            n = i
-            if m * 2 + n * 2 + 4 == brown:
-                return [m+2, n+2]
+def solution(brown:int, yellow:int) -> list[int] | None:
+    '''m의 가능한 각 값에 대해
+     m이 노란색을 나누면 n = 노란색 // m이고 
+     2 * m + 2 * n + 4 == 갈색이면 [max(m,n) + 2, min(m, n) + 2] 을 반환합니다.
+    
+    Parameters
+    ----------
+    brown : int
+        갈색 타일의 수
+    yellow : int
+        노란색 타일의 수
+    
+    Returns
+    -------
+        두 정수의 목록
+    
+    '''
+    for m in range(1, yellow+1):
+        if yellow % m == 0:
+            n = yellow // m
+            if 2 * m + 2 * n + 4 == brown:
+                return [max(m,n) + 2, min(m, n) + 2]
