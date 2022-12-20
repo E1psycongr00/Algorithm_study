15fb76066855fd2c31e8ca1c771c7dddecf36179
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-20 23:04:43 +0900

    Solved(스택): 카운트 괄호

--- 임현규/programmers/Lv2/올바른_괄호/python/카운트_괄호.py
+++ 임현규/programmers/Lv2/올바른_괄호/python/카운트_괄호.py
@@ -1,24 +1,28 @@
 OPEN_BRACKET = "("
 CLOSE_BRACKET = ")"
 
+
 def solution(s):
     count = 0
     for ch in s:
         count = countOpenBracket(count, ch)
         count = countCloseBracket(count, ch)
-        if (isInvalidCount(count)):
+        if isInvalidCount(count):
             return False
     return count == 0
 
-def countOpenBracket(count:int, ch:str) -> int:
-    if (ch == OPEN_BRACKET):
+
+def countOpenBracket(count: int, ch: str) -> int:
+    if ch == OPEN_BRACKET:
         return count + 1
     return count
 
-def countCloseBracket(count:int, ch:str) -> int:
-    if (ch == CLOSE_BRACKET):
+
+def countCloseBracket(count: int, ch: str) -> int:
+    if ch == CLOSE_BRACKET:
         return count - 1
     return count
+
 
 def isInvalidCount(count) -> bool:
     return count < 0
