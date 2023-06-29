db88c37ee148ada356d16cec750356596e225016
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-06-29 18:53:06 +0900

    Solved(구현): 오픈채팅방

---
+++ 임현규/programmers/Lv2/오픈채팅방/programmers.py
@@ -0,0 +1,18 @@
+nicknames = {}
+
+ENTER = "{}님이 들어왔습니다."
+LEAVE = "{}님이 나갔습니다."
+
+def solution(record):
+    logs = []
+    for s in record:
+        split = s.split(" ")
+        if split[0] == "Enter":
+            logs.append((ENTER, split[1]))
+            nicknames[split[1]] = split[2]
+        elif split[0] == "Leave":
+            logs.append((LEAVE, split[1]))
+        elif split[0] == "Change":
+            nicknames[split[1]] = split[2]
+    return [log.format(nicknames[id]) for log, id in logs]
+        
