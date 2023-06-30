ded8d7aeec26869c34b35db160893a085c3f985b
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-06-30 18:37:36 +0900

    Solved(구현):이중 우선 순위 큐

---
+++ 임현규/programmers/Lv3/이중우선순위큐/improved_ide2.py
@@ -0,0 +1,40 @@
+import heapq
+
+
+def solution(operations):
+    doubleHeap = DoubleHeap()
+    for operation in operations:
+        op, num = operation.split(" ")
+        num = int(num)
+        if op == "I":
+            doubleHeap.push(num)
+        elif op == "D":
+            doubleHeap.pop(num)
+    return [doubleHeap.pop(1), doubleHeap.pop(-1)]
+
+
+class DoubleHeap:
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+        self.exist = set()
+    
+    def push(self, num):
+        heapq.heappush(self.min_heap, num)
+        heapq.heappush(self.max_heap, -num)
+        self.exist.add(num)
+    
+    def pop(self, num):
+        while self.max_heap and -self.max_heap[0] not in self.exist:
+                heapq.heappop(self.max_heap)
+        while self.min_heap and self.min_heap[0] not in self.exist:
+                heapq.heappop(self.min_heap)
+        if num == 1 and self.max_heap:
+            value = -heapq.heappop(self.max_heap)
+            self.exist.remove(value)
+            return value
+        if num == -1 and self.min_heap:
+            value = heapq.heappop(self.min_heap)
+            self.exist.remove(value)
+            return value
+        return 0
