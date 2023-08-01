0a76e674598fb50b18bcff8a92d1d6ae182818e6
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2023-08-01 21:05:02 +0900

    Solved(구현): 게임맵 최단거리

--- 임현규/programmers/Lv2/게임맵최단거리/java/programmers.java
+++ 임현규/programmers/Lv2/게임맵최단거리/java/programmers.java
@@ -1,21 +1,21 @@
 import java.util.*;
 
 class Solution {
-    private static final int[] dy = new int[] {0, -1, 0, 1};
-    private static final int[] dx = new int[] {-1, 0, 1, 0};
-    
+    private static final int[] dy = new int[] { 0, -1, 0, 1 };
+    private static final int[] dx = new int[] { -1, 0, 1, 0 };
+
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
         int[][] dist = new int[n][m];
-        
+
         Queue<Point> q = new ArrayDeque<>();
         q.add(new Point(0, 0));
         dist[0][0] = 1;
-        
+
         while (!q.isEmpty()) {
             Point p = q.poll();
-            if (p.y == n -1 && p.x == m - 1) {
+            if (p.y == n - 1 && p.x == m - 1) {
                 return dist[p.y][p.x];
             }
             for (int i = 0; i < 4; ++i) {
@@ -30,14 +30,14 @@ class Solution {
                 }
             }
         }
-        
+
         return -1;
     }
-    
+
     private static class Point {
         private final int y;
         private final int x;
-        
+
         public Point(int y, int x) {
             this.y = y;
             this.x = x;
